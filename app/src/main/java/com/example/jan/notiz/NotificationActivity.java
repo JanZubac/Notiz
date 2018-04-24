fd93fd48f8496e9049108fa05a5469bff43d224a
janzubac <dat14jzu@student.lu.se>
2018-04-24 16:23:15 +0200

    Back button added

--- app/src/main/java/com/example/jan/notiz/NotificationActivity.java
+++ app/src/main/java/com/example/jan/notiz/NotificationActivity.java
@@ -7,10 +7,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EdgeEffect;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class NotificationActivity extends AppCompatActivity {
 
     Button saveButton, mapButton;
+    ImageButton backButton;
     EditText t;
 
     @Override
@@ -18,17 +20,23 @@ public class NotificationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
         t = (EditText) findViewById(R.id.activity);
+        saveButton = (Button) findViewById(R.id.savebutton);
+        backButton = (ImageButton) findViewById(R.id.back);
     }
 
     public void saveNotification(View view) {
-        saveButton = (Button) findViewById(R.id.savebutton);
-
         Intent intent = new Intent();
         intent.putExtra("editTextValue", t.getText().toString());
         setResult(RESULT_OK, intent);
         setContentView(R.layout.activity_main);
         finish();
     }
+
+    public void goBack(View view) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+    }
+
 
 
     public void clearText(View view) {
